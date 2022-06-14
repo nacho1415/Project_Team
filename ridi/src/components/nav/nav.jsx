ebd43992f1c4d538b0161b46bf8018454b865ad9
hoseol OH <dhgh9590@naver.com>
2022-06-14 15:33:07 +0900

    메인페이지 작성

--- ridi/src/components/nav/nav.jsx
+++ ridi/src/components/nav/nav.jsx
@@ -1,30 +1,79 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './nav.module.css';
 import {Link, useNavigate } from 'react-router-dom';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch,faTimesCircle,faBell,faShoppingCart,faBook,faUser} from "@fortawesome/free-solid-svg-icons";
+import { useRef } from 'react';
 
 const Nav = () => {
     let navigate = useNavigate();
+    let [active,setActive] = useState(0);
+    let [searchInput,setSearchInput] = useState(``);
+
+    //서버로 검색창 입력 값 데이터 보내기
+    function onSearch(){
+        fetch('http://localhost:8080/', {
+            method: 'post',
+            headers: {
+            "Content-Type": "application/json; charset=utf-8"	
+            },
+            body: JSON.stringify(searchInput)
+        })
+    }
+
     
     return (
         <div>
             <nav className={styles.nav}>
                 <div className={styles.container}>
-                    <div className={styles.left_menu}>
-                        <ul>
-                            <li><em>웹툰/만화</em></li>
-                            <li><em>웹소설</em></li>
-                            <li><em>도서</em></li>
-                            <li><em>셀렉트</em></li>
-                        </ul>
-                    </div>
-                    <div className={styles.right_menu}>
-                        <ul>
-                            <li><em>회원가입</em></li>
-                            <li><em>로그인</em></li>
-                        </ul>
+                    <div className={styles.menu}>
+                        <div className={styles.left_menu}>
+                            <ul>
+                                <li className={active == 0 ? `${styles.active}`:null}><em onClick={()=>{setActive(0);navigate('/');}}>웹툰/만화</em></li>
+                                <li className={active == 1 ? `${styles.active}`:null}><em onClick={()=>{setActive(1)}}>웹소설</em></li>
+                                <li className={active == 2 ? `${styles.active}`:null}><em onClick={()=>{setActive(2)}}>도서</em></li>
+                                <li className={active == 3 ? `${styles.active}`:null}><em onClick={()=>{setActive(3)}}>셀렉트</em></li>
+                            </ul>
+                        </div>
+                        <div className={styles.right_menu}>
+                            <ul>
+                                <li><em>회원가입</em></li>
+                                <li><em onClick={()=>{navigate('/login')}}>로그인</em></li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </nav>
+            <div className={styles.sub_menu}>
+                <div className={styles.container}>
+                    <div className={styles.logo}>
+                        <p>RIDI</p>
+                        <p>WEBTOON</p>
+                    </div>
+                    <div className={styles.icon}>
+                        <div className={styles.search}>
+                            <form>
+                                <input type="text" value={searchInput} onChange={(e)=>{setSearchInput(e.target.value)}}/>
+                                <FontAwesomeIcon icon={faSearch} className={styles.search_icon1}/>
+                                {
+                                    searchInput != `` ?
+                                    <FontAwesomeIcon icon={faTimesCircle} className={styles.search_icon2} onClick={()=>{setSearchInput(``)}} />
+                                    : null
+                                }
+                                <button onClick={(e)=>{e.preventDefault();onSearch();}}></button>
+                            </form>
+                        </div>
+                        <div className={styles.sub_icon}>
+                            <ul>
+                                <li><FontAwesomeIcon icon={faBell} /></li>
+                                <li><FontAwesomeIcon icon={faShoppingCart} /></li>
+                                <li><FontAwesomeIcon icon={faBook} /></li>
+                                <li><FontAwesomeIcon icon={faUser} /></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
