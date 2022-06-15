b5b342b42a239ebb724baf12cad0f1e273b60ccb
이민서 <sola2014@naver.com>
2022-06-15 13:25:33 +0900

    병합용 커밋

--- app.js
+++ app.js
@@ -1,54 +1,75 @@
+const { application } = require('express');
 const express = require('express');
-const cookieParser = require('cookie-parser');
-const morgan = require('morgan');
-const path = require('path');
 const session = require('express-session');
-const nunjucks = require('nunjucks');
-const dotenv = require('dotenv');
+app = express();
+app.use(express.json());
+var cors = require('cors');
+app.use(cors());
+const bcrypt = require('bcrypt')
+const bodyParser= require('body-parser')
+// const passport = require('passport')
+app.use(bodyParser.urlencoded({extended: true})) 
+// app.use(
+//     session({
+//         secret: 'Hello',
+//         resave: true,
+//         saveUninitialized: true,
+//     })
+// );
+authRouter = require('./routes/auth');
 
-dotenv.config();
-const pageRouter = require('./routes/page');
 
-const app = express();
-app.set('port', process.env.PORT || 8001);
-app.set('view engine', 'html');
-nunjucks.configure('views', {
-    express: app,
-    watch: true,
+let db;
+const MongoClient = require('mongodb').MongoClient;
+MongoClient.connect('mongodb+srv://minseo:lee2030@cluster0.nfdv5vj.mongodb.net/?retryWrites=true&w=majority', (err, client) => {
+    app.listen(8080, () => {
+        db = client.db('Ridi');
+        console.log('listening on 8080');
+    });
 });
 
-app.use(morgan('dev')); // 클라이언트에서 무슨 요청이 왔는지 shell에서 확인가능
-app.use(express.static(path.join(__dirname, 'public'))); // 보안용. 실제 파일 경로 대신 가짜 파일 경로로 설정
-app.use(express.json()); // 바디 파싱을 편하게 client가 json을 서버에 보냈을 때 req.body에 json을 넣어줌1
-app.use(express.urlencoded({ extended: true })); // 바디 파싱을 편하게2 client가 form을 보냇을 때 req.body에 넣어줌 
-app.use(cookieParser(process.env.COOKIE_SECRET)); // cookieParser를 사용해 쿠키 조작을 편하게
-app.use(session({   // 개인마다 저장 공간의 데이터를 바꿔주는 용도
-    resave: false,
-    saveUninitialized: false,
-    secret: process.env.COOKIE_SECRET,
-    cookie: {
-        httpOnly: true,  // 자바스크립트를 이용한 공격 방지
-        secure: false,
-    },
-}));
 
-app.use('/', pageRouter);
-
-
-
-app.use((req, res, next) => {
-    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
-    error.status = 404;
-    next(error);
-});
-
-app.use((err, req, res, next) => {
-    res.locals.message = err.message;
-    res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
-    res.status(err.status || 500);
-    res.render('error');
-});
-
-app.listen(app.get('port'), () => {
-    console.log(app.get('port'), '번 포트에서 대기중')
+app.post('/login', async (req, res, next) => {
+    const { id, paw } = req.body;
+    try {
+        const exUser = await db.collection('User').findOne({id : id})
+        if(exUser) {
+            const result = await bcrypt.compare(paw, exUser.password )
+            if(result) {
+                res.json({ id: id, comment : "o 로그인이 허가되었습니다"})
+                req.login(exUser, () => {
+                    console.log("sdadadaw")
+                })
+                console.log("되나");
+            } else {
+                res.json({ comment : "x 비밀번호가 일치하지 않습니다" })
+            }
+        } else {
+            res.json({ comment : "x 가입되지 않은 회원입니다." })
+        }
+    } catch(e){
+        console.log(e)
+    }
 })
+
+
+// app.post('/login', async (req, res, next) => {
+
+//     const { id, paw } = req.body;
+//     try {
+//         const exUser = await db.collection('User').findOne({id : id})
+//         console.log(exUser)
+//         if(exUser) {
+//             return res.json('x 아이디 중복')
+//         }
+//         const hash = await bcrypt.hash(paw, 12);
+//         await db.collection('User').insertOne({
+//             id: id,
+//             password: hash
+//         })
+//         console.log("아이디 생성완료")
+//     }catch(e) {
+//         console.log(e)
+//         res.json("x 이유 특정 불가");
+//     }
+// })
