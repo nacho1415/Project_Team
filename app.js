533c0c337ab6cfe2ff453c97ce89f7c80ca5d83a
이민서 <sola2014@naver.com>
2022-06-17 15:03:45 +0900

    DB 변경(mongoDB >> SQL[sequlize orm 이용])

--- app.js
+++ app.js
@@ -1,24 +1,17 @@
-const { application } = require('express');
 const express = require('express');
-const session = require('express-session');
 app = express();
 app.use(express.json());
 var cors = require('cors');
-app.use(cors());
+const { sequelize, User } = require('./models');
 const bcrypt = require('bcrypt')
 const bodyParser= require('body-parser');
-const { json } = require('body-parser');
-app.use(bodyParser.urlencoded({extended: true})) 
-// const passport = require('passport')
-// app.use(
-//     session({
-//         secret: 'Hello',
-//         resave: true,
-//         saveUninitialized: true,
-//     })
-// );
-authRouter = require('./routes/auth');
 
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true})) 
+sequelize.sync({ force: false })
+ .then(() => {
+    console.log('sql 데이터베이스 연결 성공');
+ })
 
 let db;
 const MongoClient = require('mongodb').MongoClient;
@@ -32,11 +25,10 @@ MongoClient.connect('mongodb+srv://minseo:lee2030@cluster0.nfdv5vj.mongodb.net/?
 app.post('/login', async (req, res) => {
     const { id, paw } = req.body
     try {
-        const exUser = await db.collection('User').findOne({id: id}); 
+        const exUser = await User.findOne({ where: {id: id } })
         if(exUser){
             const result = await bcrypt.compare(paw, exUser.password )
             if(result){
-                exUser.id
                 res.json({done: true, comment: "로그인에 성공했습니다", id: exUser.id, username: exUser.username })
                 console.log("성공확인")
             } else {
@@ -55,7 +47,7 @@ app.post('/join', async (req, res, next) => {
 
     const { userId, userPw, userEmail, userName, userAge, genderCheck} = req.body;
     try {
-        const exUser = await db.collection('User').findOne({id : userId})
+        const exUser = await User.findOne({ where: { id: userId } });
         if(!exUser) {
             let legPass =/^(?=.*[a-zA-Z])(?=.*[!@#$%^*+~=-])(?=.*[0-9]).{8,25}$/
             if(legPass.test(userPw)){
@@ -63,14 +55,13 @@ app.post('/join', async (req, res, next) => {
                     var regExp =/^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/
                     if(regExp.test(userEmail)){
                         const hash = await bcrypt.hash(userPw, 12);
-                        await db.collection('User').insertOne({
-                            id: userId,
-                            password: hash,
-                            email: userEmail,
-                            username: userName,
-                            birth_year: userAge,
-                            gender: genderCheck
-                        });
+                        await User.create({
+                            id : userId,
+                            username : userName,
+                            password : hash,
+                            birth_year : userAge,
+                            gender : genderCheck
+                        })
                         console.log("확인용");
                         res.json({done: true, comment: "아이디 생성 완료됐습니다."});
                     }else {
