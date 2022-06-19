a9973c3d04ca552b748fa797c376e112daa55c79
이민서 <sola2014@naver.com>
2022-06-19 18:48:56 +0900

    로그인API, 로그아웃API 가독성 개선

--- routes/join.js
+++ routes/join.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt')
 
 const User = require('../models/user');
 
@@ -10,34 +10,30 @@ router.post('/', async (req, res, next) => {
     const { userId, userPw, userEmail, userName, userAge, genderCheck} = req.body;
     try {
         const exUser = await User.findOne({ where: { id: userId } });
-        if(!exUser) {
-            let legPass =/^(?=.*[a-zA-Z])(?=.*[!@#$%^*+~=-])(?=.*[0-9]).{8,25}$/
-            if(legPass.test(userPw)){
-                if(userAge.length == 4 && Number(userAge)){
-                    var regExp =/^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/
-                    if(regExp.test(userEmail)){
-                        const hash = await bcrypt.hash(userPw, 12);
-                        await User.create({
-                            id : userId,
-                            username : userName,
-                            password : hash,
-                            birth_year : userAge,
-                            gender : genderCheck
-                        })
-                        console.log("확인용");
-                        res.json({done: true, comment: "아이디 생성 완료됐습니다."});
-                    }else {
-                        res.json({done: false, comment: "올바른 이메일 형식이 아닙니다."})
-                    }
-                } else {
-                    res.json({done: false, comment: "x 출생년도 형식이 잘못됐습니다"});
-                }
-            }else{
-                res.json({done: false, comment: "비밀번호 유효성 검사 통과 실패"});
-            }
-        } else {
-            res.json({done: false, comment: "아이디 중복됐습니다."});
+        if(exUser){
+            return res.json({done: false, comment: "아이디 중복됐습니다."});
+        } 
+        let legPass =/^(?=.*[a-zA-Z])(?=.*[!@#$%^*+~=-])(?=.*[0-9]).{8,25}$/
+        if(!legPass.test(userPw)){
+            return res.json({done: false, comment: "비밀번호 유효성 검사 통과 실패"});
         }
+        if(!userAge.length == 4 || !Number(userAge)) {
+            return res.json({done: false, comment: "x 출생년도 형식이 잘못됐습니다"})
+        }
+        var regExp =/^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/
+        if(!regExp.test(userEmail)) {
+            return res.json({done: false, comment: "올바른 이메일 형식이 아닙니다."})
+        }
+        const hash = await bcrypt.hash(userPw, 12);
+        await User.create({
+            id : userId,
+            username : userName,
+            password : hash,
+            birth_year : userAge,
+            gender : genderCheck
+        })
+        console.log("확인용");
+        res.json({done: true, comment: "아이디 생성 완료됐습니다."});
     } catch(e){
         console.log(e)
         res.json({done: false, comment: "에러처리가 안된 에러가 발생했습니다."});
